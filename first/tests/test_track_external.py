df2339a2faa65c353add121206a5199bde2bdee5
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-01 02:12:27 -0300

    Ajuste dos testes

--- first/tests/test_track_external.py
+++ first/tests/test_track_external.py
@@ -4,5 +4,5 @@ spotfy = Spotfy()
 
 def test_track_external():
   tracks = spotfy.get_music_tracks('4xKZLaPLh99LGKRjRlHxyX?si=83c968361f034f16')
-  print (type(tracks))
+  assert type(tracks) == dict
   
