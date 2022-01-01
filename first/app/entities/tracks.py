b0ddaffaac683c5bb48197becd86d87f75798da6
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-01 02:12:46 -0300

    Add regras de negócio

--- first/app/entities/tracks.py
+++ first/app/entities/tracks.py
@@ -1,10 +1,23 @@
 from app.entities.interfaces.tracks import TracksModel
 
-class Tracks(TracksModel):
+class Playlist(TracksModel):
   
   def __init__(self, musics: list):
     self._musics = musics
     
-  @property
-  def musics(self):
-    return self._musics
+  def _get_track_style(self, climate: int):
+    if climate > 30:
+      return 'party'
+    if climate >= 15 and climate <= 30:
+      return 'pop'
+    if climate >= 10 and climate <= 14:
+      return 'rock'
+    return 'classic'
+    
+  def musics(self, climate: int):
+    music_list = []
+    for music in self._musics:
+      if music['style'] == self._get_track_style(climate):
+        music_list.append(music)
+    return music_list
+  
