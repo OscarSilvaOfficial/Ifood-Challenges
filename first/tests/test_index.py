e6b828a7c8f6e45b8c653f176c145c96af42de24
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-01 10:54:02 -0300

    Ajuste no path do teste

--- first/tests/test_index.py
+++ first/tests/test_index.py
@@ -4,6 +4,5 @@ from app.infra import mount_app
 client = TestClient(mount_app())
   
 def test_get_index_message():
-  response = client.get('/')
-  print(response.json())
+  response = client.get('/api')
   assert response.status_code == 200
