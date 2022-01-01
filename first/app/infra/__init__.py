2379133de0538be0e63ad9c4200c8ffe1c020760
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-01 10:53:06 -0300

    Separação de config das rotas

--- first/app/infra/__init__.py
+++ first/app/infra/__init__.py
@@ -1,9 +1,18 @@
 from app.infra.main import MainServer
 from app.infra.http.fastapi import FastAPIServer
 from app.infra.routes.main_routes import main_routes
+from fastapi import APIRouter
+
+def mount_routes(app, router=None):
+  router = APIRouter(
+    prefix="/api",
+    responses={404: {"description": "Not found"}},
+  )
+  routes = main_routes(router)
+  app.include_router(routes)
 
 def mount_app():
   server = MainServer(FastAPIServer)
   app = server.app
-  main_routes(app)
+  mount_routes(app)
   return app
